e79ad5de13bc0aa44e79af4134eda4cb1b2fd0ff
charanck <charank804.com>
2024-06-25 11:14:35 +0530

    Created health handler for grpc and http and basic server setup

--- cmd/server.go
+++ cmd/server.go
@@ -1,1 +1,42 @@
 package cmd
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+
+	grpchandler "github.com/charanck/ABAC/internal/handler/grpc"
+	abac "github.com/charanck/ABAC/protobuf/generated"
+	"github.com/labstack/echo/v4"
+	"google.golang.org/grpc"
+)
+
+const grpcPort = 3001
+
+func StartServer() {
+	// Start http server
+	go func() {
+		e := echo.New()
+		e.GET("/", func(c echo.Context) error {
+			return c.JSON(http.StatusOK, abac.HealthResponse{
+				Message: "OK",
+			})
+		})
+
+		if err := e.Start(":3000"); err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	// Start grpc server
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	abac.RegisterHealthServer(grpcServer, grpchandler.HealthServer{})
+	grpcServer.Serve(lis)
+
+}
