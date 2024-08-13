f3bc757a16473c7aa50cd2acaf203aa227898ba9
charanck <charank804.com>
2024-08-13 22:17:45 +0530

    Setup grpc codegen and updated the readme doc

--- go.mod
+++ go.mod
@@ -7,8 +7,8 @@ require (
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/labstack/echo/v4 v4.12.0
 	github.com/mattn/go-sqlite3 v1.14.22
-	google.golang.org/grpc v1.64.0
-	google.golang.org/protobuf v1.33.0
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.34.2
 )
 
 require (
@@ -27,13 +27,13 @@ require (
 	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/mod v0.17.0 // indirect
-	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
-	golang.org/x/tools v0.21.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- script/script.go
+++ script/script.go
@@ -8,7 +8,6 @@ import (
 
 func main() {
 	for {
-
 		fmt.Println("Select below option:")
 		fmt.Println("1) generate http server code")
 		fmt.Println("2) generate grpc server code")
@@ -20,21 +19,41 @@ func main() {
 		}
 		switch input {
 		case 1:
-			fmt.Println("generating http server code")
-			cmd := exec.Command("go", "run", "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen", "-config=./codegen-config.yaml", "./openapi.json")
-			_, err := cmd.Output()
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
-			fmt.Println("code generation done")
+			generateHTTPServerCode()
 			return
 		case 2:
-			fmt.Println("NOT IMPLEMENTED YET")
+			generateGRPCServerCode()
 			return
 		default:
 			fmt.Println("invalid option")
 			time.Sleep(1 * time.Second)
 		}
 	}
+}
+
+func generateHTTPServerCode() {
+	fmt.Println("generating http server code")
+	cmd := exec.Command("go", "run", "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen", "-config=./codegen-config.yaml", "./openapi.json")
+	_, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err.Error())
+		fmt.Println("http code generation failed")
+		return
+	}
+	fmt.Println("code generation done")
+}
+
+func generateGRPCServerCode() {
+	fmt.Println("generating grpc server code")
+
+	cmd := exec.Command("protoc", "--go_out=./protobuf/generated", "--go-grpc_out=./protobuf/generated", "./protobuf/abac.proto")
+	cmd.Dir = "../"
+	x, err := cmd.Output()
+	fmt.Println(string(x))
+	if err != nil {
+		fmt.Println(err.Error())
+		fmt.Println("grpc code generation failed")
+		return
+	}
+	fmt.Println("grpc code generation done")
 }
