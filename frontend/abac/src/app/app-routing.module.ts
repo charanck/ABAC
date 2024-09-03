19b75dca5d4b4c27e7c23c7b6506609f46179f8f
charanck <charank804.com>
2024-09-03 10:35:34 +0530

    Added rosource navigation button

--- frontend/abac/src/app/app-routing.module.ts
+++ frontend/abac/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResourcesComponent } from './resources/resources.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "resources",
+    component: ResourcesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
