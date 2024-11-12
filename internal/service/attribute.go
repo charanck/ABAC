98d54264403058f7ad415218fe62e5b53476c0ae
charanck <charank804.com>
2024-11-12 21:17:01 +0530

    feat: implemented create attribute service

--- internal/service/attribute.go
+++ internal/service/attribute.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"time"
+
+	"github.com/charanck/ABAC/internal/model"
+	"github.com/charanck/ABAC/internal/repository"
+	"github.com/google/uuid"
+)
+
+type Attribute struct {
+	repository *repository.Attribute
+}
+
+func NewAttribute(repository *repository.Attribute) Attribute {
+	return Attribute{
+		repository: repository,
+	}
+}
+
+func (a *Attribute) Create(attribute model.Attribute) (string, error) {
+	attribute.Id = uuid.New().String()
+	attribute.Created = time.Now()
+	attribute.Updated = time.Now()
+	return a.repository.Create(attribute)
+}
